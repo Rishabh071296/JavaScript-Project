f20ed39792f4c2afc1a452657ed8c51049ddc559
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-07-28 12:46:34 +0530

    Update

---
+++ Project 3/script.js
@@ -0,0 +1,6 @@
+cosnt = document.querySelector('#clock')
+setInterval(function(){
+    let date = new Date ()
+    // console.log(date.toLocaleTimeString);
+    clock.innerHTML = date.toLocaleTimeString();
+},1000)
